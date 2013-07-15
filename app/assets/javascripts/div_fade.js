5694bf60b742c4f2eb1f7ebb693b0d58f55981fc
SteinerHunter <steinerhunter@gmail.com>
2013-07-16 00:25:26 +0300

    Added back button with fade to main menu.

--- app/assets/javascripts/div_fade.js
+++ app/assets/javascripts/div_fade.js
@@ -7,8 +7,20 @@ $(document).ready(function(){
     });
     $('#SendSomething').click(function(e){
         e.preventDefault();
-        $('#main_div').fadeOut('fast', function(){
-            $('#send_div').fadeIn('fast');
+        $('#main_div').fadeOut('slow', function(){
+            $('#send_div').fadeIn('slow');
+        });
+    });
+    $('#back_pass_id').click(function(e){
+        e.preventDefault();
+        $('#pass_div').fadeOut('slow', function(){
+            $('#main_div').fadeIn('slow');
+        });
+    });
+    $('#back_send_id').click(function(e){
+        e.preventDefault();
+        $('#send_div').fadeOut('slow', function(){
+            $('#main_div').fadeIn('slow');
         });
     });
 });

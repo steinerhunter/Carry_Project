ff806984c4b7f5b5f631f5eacb3d65755319faf0
SteinerHunter <steinerhunter@gmail.com>
2013-07-22 00:38:17 +0300

    Added dynamic add of comments to suggest/request.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ActiveRecord::Base
   attr_accessible :commentable_id, :commentable_type, :content
   belongs_to :commentable, :polymorphic => true
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :content, :presence => { :message => "Got nothing to add?"}
+
 end

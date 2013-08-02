75cb00736a4ded78795eccbf19fedff9c3864ee4
SteinerHunter <steinerhunter@gmail.com>
2013-08-02 15:14:42 +0300

    Changed config.assets.compile to true.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ TheCarryProject::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -46,7 +46,7 @@ TheCarryProject::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( formToWizard.js bootstrap-datepicker.js div_fade.js popup.js flash_fade.js Close.png)
+  config.assets.precompile += %w( formToWizard.js bootstrap-datepicker.js div_fade.js popup.js flash_fade.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false

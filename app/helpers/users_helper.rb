a26928a4436c6894c95601ae6b13eb2e4a882c4e
SteinerHunter <steinerhunter@gmail.com>
2013-08-11 00:30:41 +0300

    Completely revampled header.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -12,4 +12,10 @@ module UsersHelper
     image_tag(gravatar_url, alt: user.name, class:"gravatar")
   end
 
+  def header_gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=30&d=wavatar"
+    image_tag(gravatar_url, alt: user.name, class:"header_gravatar")
+  end
+
 end

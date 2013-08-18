8b674b6dbbdef45ad0f89b818f3a6c67c79f1647
SteinerHunter <steinerhunter@gmail.com>
2013-08-18 23:24:57 +0300

    Testing email in production.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 TheCarryProject::Application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.smtp_settings = {
+    :address            => "smtp.sendgrid.net",
+    :port                   => 587,
+    :domain              => "sendwith.me",
+    :user_name       =>  ENV['SENDGRID_USERNAME'],
+    :password         =>  ENV['SENDGRID_PASSWORD'],
+    :authentication       => :plain,
+    :enable_starttls_auto => true
+}
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,10 @@ TheCarryProject::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  # Mailer definitions
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log

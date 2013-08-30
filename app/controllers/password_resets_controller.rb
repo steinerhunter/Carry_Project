85ead0715fe44660abd7675df20c1127a71ba326
SteinerHunter <steinerhunter@gmail.com>
2013-08-30 19:09:28 +0300

    Trying to add validation to password change.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -18,9 +18,8 @@ class PasswordResetsController < ApplicationController
     if @user.password_reset_sent_at < 2.hours.ago
       redirect_to new_password_reset_path, :alert => "Password reset has expired."
     elsif @user.update_attributes(params[:user])
-      redirect_to root_url, :notice => "Password has been reset."
-    else
-      render :edit
+      flash[:confirm] = "Password has been reset."
+      respond_with(@user, :location => root_path)
     end
   end
 

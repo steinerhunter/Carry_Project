815ee866ba98ed30dba4665178fb6775b8925ab0
SteinerHunter <steinerhunter@gmail.com>
2013-09-15 23:45:12 +0300

    This should work now.

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,11 +1,17 @@
 class ConfirmationsController < ApplicationController
-  # GET /confirm/<token>
   def confirm
     @user = User.find_by_email_confirmation_token(params[:token])
       if @user.present?
-        flash[:profile_update] = "You've successfully confirmed your email address!"
-        @user.confirm_user
-        sign_in @user
-    end
+        if @user.email_confirmed
+          flash[:profile_update] = "You're already confirmed your email address."
+        else
+          flash[:profile_update] = "You've successfully confirmed your email address!"
+          @user.confirm_user
+          sign_in @user
+        end
+      else
+        flash[:profile_update] = "Sorry, that's an invalid confirmation token."
+      end
+    redirect_to root_path
   end
 end

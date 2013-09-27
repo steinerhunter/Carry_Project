65fe49d41516a0c773efa2fe43773ece7dd59c5d
SteinerHunter <steinerhunter@gmail.com>
2013-09-27 15:42:56 +0300

    Trying to raise error to yaml.

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -9,6 +9,7 @@ class AuthenticationsController < ApplicationController
   end
 
   def create
+    raise request.env["omniauth.auth"].to_yaml
     omniauth = request.env["omniauth.auth"]
     authentication = Authentication.find_by_provider_and_uid(omniauth[:provider], omniauth[:uid])
     user = User.find_by_email(omniauth[:info][:email])

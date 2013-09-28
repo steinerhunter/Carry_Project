0bcca95fffee59a937c741b14ce3c1fd3db2ce59
SteinerHunter <steinerhunter@gmail.com>
2013-09-28 11:12:05 +0300

    Added FB profile image to profile page.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -27,6 +27,9 @@ module UsersHelper
     if type == "header"
       split_photo_url = photo_url[0].split("?")[0] << "?width=20&height=20"
       image_tag(split_photo_url, alt: current_user.name, class:"header_gravatar")
+    elsif type == "profile"
+      split_photo_url = photo_url[0].split("?")[0] << "?width=100&height=100"
+      image_tag(split_photo_url, alt: current_user.name, class:"header_gravatar")
     end
   end
 

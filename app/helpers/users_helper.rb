e3c1a55a694793a6b380ddaaee2219c21b84ac7f
SteinerHunter <steinerhunter@gmail.com>
2013-09-28 15:44:04 +0300

    Fixed some CSS stuff.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@ module UsersHelper
   # Returns a Gravatar for a given user (Thanks to http://gravatar.com)
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?d=wavatar"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=100&d=wavatar"
     image_tag(gravatar_url, alt: user.name, class:"gravatar")
   end
 

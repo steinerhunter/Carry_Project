3cef64ab85e14b9dce02261d5fed1caf41d4b893
SteinerHunter <steinerhunter@gmail.com>
2013-10-11 00:12:15 +0300

    Changed amount to integer.

--- db/migrate/20131010163215_create_payments.rb
+++ db/migrate/20131010163215_create_payments.rb
@@ -2,7 +2,7 @@ class CreatePayments < ActiveRecord::Migration
   def change
     create_table :payments do |t|
       t.string :status
-      t.float :amount
+      t.integer :amount
       t.string :transaction_number
 
       t.timestamps

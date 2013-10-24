0e2a5fa550761e086ae88690aae3ea5b5fd38921
SteinerHunter <steinerhunter@gmail.com>
2013-10-25 01:32:55 +0300

    Changed payKey to preapprovalKey in request_payment model.

--- app/models/request_payment.rb
+++ app/models/request_payment.rb
@@ -1,10 +1,10 @@
 class RequestPayment < ActiveRecord::Base
-  attr_accessible :user_id, :request_delivery_id, :payKey, :status
+  attr_accessible :user_id, :request_delivery_id, :preapprovalKey, :status
   belongs_to :user
 
   validates :user_id, :presence => { :message => "Must be owned by some user..."}
   validates :request_delivery_id, :presence => { :message => "Must be associated with a certain request delivery..."}
-  validates :payKey, :presence => { :message => "Must have a preapprovalKey..."}
+  validates :preapprovalKey, :presence => { :message => "Must have a preapprovalKey..."}
   validates :status, :presence => { :message => "Must have a status..."}
 
 end

6b4bfd41f0c71e016cb0a69e621c4c548552b6cd
SteinerHunter <steinerhunter@gmail.com>
2013-10-26 01:28:47 +0300

    Added reviews after task completion.

--- app/controllers/user_reviews_controller.rb
+++ app/controllers/user_reviews_controller.rb
@@ -15,7 +15,28 @@ class UserReviewsController < ApplicationController
     if @user_review.save
       @reviewed_user = User.find_by_id(@user_review.to_user_id)
       @reviewed_user.add_review
-      respond_with(@user_review, :location => root_path)
+      if @user_review.job_type == "SENDER"
+        if @user_review.req_or_sugg == "suggest_delivery"
+          @suggest_delivery = SuggestDelivery.find_by_id(@user_review.task_id)
+          @suggest_delivery.sender_reviewed = true
+          @suggest_delivery.save
+        elsif @user_review.req_or_sugg == "request_delivery"
+          @request_delivery = RequestDelivery.find_by_id(@user_review.task_id)
+          @request_delivery.sender_reviewed = true
+          @request_delivery.save
+        end
+      elsif @user_review.job_type == "TRANSPORTER"
+        if @user_review.req_or_sugg == "suggest_delivery"
+          @suggest_delivery = SuggestDelivery.find_by_id(@user_review.task_id)
+          @suggest_delivery.transporter_reviewed = true
+          @suggest_delivery.save
+        elsif @user_review.req_or_sugg == "request_delivery"
+          @request_delivery = RequestDelivery.find_by_id(@user_review.task_id)
+          @request_delivery.transporter_reviewed = true
+          @request_delivery.save
+        end
+        respond_with(@user_review, :location => root_path)
+      end
     end
   end
 

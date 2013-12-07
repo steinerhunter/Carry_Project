5fa589316f18e6e379f9a858673b273c63e6f2b1
SteinerHunter <steinerhunter@gmail.com>
2013-12-07 19:44:59 +0200

    Fixed bugs 12,16.

--- app/controllers/phone_verifications_controller.rb
+++ app/controllers/phone_verifications_controller.rb
@@ -5,15 +5,15 @@ class PhoneVerificationsController < ApplicationController
     if @phone.present?
       if @phone.verified
         flash[:failure] = "Sorry!<br><div class='sub_flash_text'>You've already confirmed your phone number.</div>".html_safe
-        redirect_to user_path(@user.id)
+        redirect_to  session[:return_to] || root_path
       else
-        flash[:success] = "Thank you!<br><div class='sub_flash_text'>Your phone number has been successfully verified..</div>".html_safe
+        flash[:success] = "Thank you!<br><div class='sub_flash_text'>Your phone number has been successfully verified.</div>".html_safe
         @phone.confirm_phone
-        redirect_to user_path(@user.id)
+        redirect_to  session[:return_to] || root_path
       end
     else
       flash[:failure] = "Sorry!<br><div class='sub_flash_text'>That's an invalid verification code.</div>".html_safe
-      redirect_to root_path
+      redirect_to  session[:return_to] || root_path
     end
   end
 end

8058b69606c01024060511a3823a3c86bb341a36
SteinerHunter <steinerhunter@gmail.com>
2014-01-09 18:03:22 +0200

    Fixed Chrome bug, changed submit button text to 'please wait'.

--- app/assets/javascripts/please_wait.js
+++ app/assets/javascripts/please_wait.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
-    $('.edit_update').on('click','.update_details',function () {
-        $(this).removeClass('update_details')
-        $(this).addClass('please_wait')
-        $(this).attr("disabled", true);
+    $('.edit_picture').on('click','.update_details',function () {
+        $(this).removeClass('update_details');
+        $(this).addClass('please_wait');
+        $(this).prop("value", "Please wait...");
+        $(this).prop("disabled", true);
+        $('#edit_picture').submit()
     });
 });

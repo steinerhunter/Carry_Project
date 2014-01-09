74220eee0895f9296e168177194709249b2c2511
SteinerHunter <steinerhunter@gmail.com>
2014-01-09 18:27:48 +0200

    Fixed bug where submit changes only when actual submit happens.

--- app/assets/javascripts/please_wait.js
+++ app/assets/javascripts/please_wait.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
-    $('.edit_picture').on('click','.update_details',function () {
+    $('.edit_update').on('click','.update_details',function () {
         $(this).removeClass('update_details');
         $(this).addClass('please_wait');
         $(this).prop("value", "Please wait...");
         $(this).prop("disabled", true);
-        $('#edit_picture').submit()
+        if($('#new_request_delivery').length) {
+            $('#new_request_delivery').submit()
+        }
+        else if($('#edit_picture').length) {
+            $('#edit_picture').submit()
+        }
     });
 });
